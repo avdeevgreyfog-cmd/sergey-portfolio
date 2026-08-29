23a33933a492eeae8b2ed10999b757f4fd4e2f14
avdeevgreyfog-cmd <avdeevgreyfog@gmail.com>
2026-08-29 21:27:59 +0300

    Remove obsolete V3 media runtimes

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,8 @@ import { initProjectProof } from './portfolio/project-proof.js';
 
 initNavigation(); observeReveals(); initProjectProof();
 const route=routePath();
-if(route==='/') import('./portfolio/home.js').then((m)=>m.initHome());
 if(route==='/works/'||route==='/works') import('./portfolio/works.js').then((m)=>m.initWorks());
 if(route==='/contact/'||route==='/contact') import('./portfolio/contact.js').then((m)=>m.initContact());
-if(route==='/work/raznye-ludi/'||route==='/work/raznye-ludi') import('./portfolio/case-raznye.js').then((m)=>m.initRaznyeCase());
 if(route.includes('/effects/video-scroll')) import('./effects/video-scroll.js').then((m)=>m.initVideoScroll());
 if(route.includes('/effects/scroll-story')) import('./effects/scroll-story.js').then((m)=>m.initScrollStory());
 if(route.includes('/effects/kinetic-type')) import('./effects/kinetic-type.js').then((m)=>m.initKineticType());
